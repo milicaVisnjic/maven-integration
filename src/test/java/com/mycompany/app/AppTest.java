2658b2e5b908bc4fb63f29053df659e5db5ac40b
Mauricio Cano <mcano.programas@gmail.com>
2020-06-22 20:20:23 +0200

    sonarcloud v1.1

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -17,4 +17,15 @@ public class AppTest
 	    App palindromeTester = new App();
 	    assertTrue(palindromeTester.isPalindrome(""));
 	}
+	@Test
+	public void whenPalindrom_thenAccept() {
+	    Palindrome palindromeTester = new Palindrome();
+	    assertTrue(palindromeTester.isPalindrome("noon"));
+	}
+	     
+	@Test
+	public void whenNearPalindrom_thanReject(){
+	    Palindrome palindromeTester = new Palindrome();
+	    assertFalse(palindromeTester.isPalindrome("neon"));
+	}
 }
